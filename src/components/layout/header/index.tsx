d6c5dcdf450fe1d6a7764bdafc0fca6c872c9d05
mark savka <mark.savka.it@gmail.com>
2022-08-02 14:40:27 +0300

    add ts-alias & logo

--- src/components/layout/header/index.tsx
+++ src/components/layout/header/index.tsx
@@ -1,11 +1,12 @@
 import React from "react";
+import logo from "assets/svg/logo.svg";
 
 export interface IProps {}
 
 export const Header: React.FC<IProps> = () => {
   return (
-    <div>
-      <header>header</header>
-    </div>
+    <header className='bg-gray-mintify-100'>
+      <img src={logo} alt='logo' />
+    </header>
   );
 };
